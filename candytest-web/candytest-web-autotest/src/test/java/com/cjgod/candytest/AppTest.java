9b874f2c4e61cf2047357f901c2e2f9b47f93886
kelydia <4522692@qq.com>
2015-12-17 17:07:09 +0800

    第一次提交

---
+++ candytest-web/candytest-web-autotest/src/test/java/com/cjgod/candytest/AppTest.java
@@ -0,0 +1,17 @@
+package com.cjgod.candytest;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+	@Test
+	public void testApp(){
+		Assert.assertTrue(true);
+	}
+}
+
